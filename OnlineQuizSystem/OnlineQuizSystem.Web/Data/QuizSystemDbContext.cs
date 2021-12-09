b9cc9b7fe5bc480e17019c270e6fd6903aa9ecb2
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2021-12-09 13:49:41 +0200

    Clead project from useless code

---
+++ OnlineQuizSystem/OnlineQuizSystem.Web/Data/QuizSystemDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineQuizSystem.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    public class QuizSystemDbContext : IdentityDbContext
+    {
+        public QuizSystemDbContext(DbContextOptions<QuizSystemDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
